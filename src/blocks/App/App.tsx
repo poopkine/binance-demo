ec44538bda3dd87519fc6df0fff1bc65216e1d72
poopkine <poopkine@ya.ru>
2018-05-12 20:01:44 +0300

    Implement portfolio balance

--- src/blocks/App/App.tsx
+++ src/blocks/App/App.tsx
@@ -15,6 +15,7 @@ export interface IAppProps {
 
 export interface IAppState {
   title: string;
+  balance: object;
 }
 
 export default class App extends Block<IAppProps, IAppState> {
@@ -24,12 +25,18 @@ export default class App extends Block<IAppProps, IAppState> {
     super(props);
 
     this.state = {
-      title: 'Initializing'
+      title: 'Initializing',
+      balance: {}
     };
   }
 
   public componentDidMount() {
-    this.setState({ title: 'Binance Demo' });
+    const data = this.getBinanceData();
+
+    this.setState({
+      title: 'Binance Demo',
+      balance: data.balance
+    });
   }
 
   protected content() {
@@ -37,10 +44,32 @@ export default class App extends Block<IAppProps, IAppState> {
       <Fragment>
         <Head title={this.state.title}/>
         <Body>
-          <Portfolio/>
+          <Portfolio balance={this.state.balance}/>
         </Body>
         <Foot/>
       </Fragment>
     );
+  }
+
+  private getBinanceData() {
+    const balance = {
+      BTC: { available: '0.00000000', onOrder: '0.00000000' },
+      ETH: { available: '0.01929634', onOrder: '0.00000000' },
+      BNB: { available: '0.99813163', onOrder: '0.00000000' }
+    };
+
+    return {
+      balance: this.removeEmptyFromBalance(balance)
+    };
+  }
+
+  private removeEmptyFromBalance(balance) {
+    return Object.keys(balance).reduce((acc, item) => {
+      if (parseFloat(balance[item].available) > 0) {
+        acc[item] = balance[item];
+      }
+
+      return acc;
+    }, {});
   }
 }
